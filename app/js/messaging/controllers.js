5c7c5fd3f34aa1a07fd7f07fa703f06ac7f4cc18
chrisaru <chrisaru@student.matnat.uio.no>
2014-11-14 04:43:35 +0100

    Kan søke opp, lagre og slette personer i new (til sending av mail). Ser ikke pent ut enda. Git sutrer om trailing whitespace. Kommentering er så 2013.

--- app/js/messaging/controllers.js
+++ app/js/messaging/controllers.js
@@ -1,61 +1,77 @@
 'use strict';
 
 var module = angular.module('overdressed.messaging.controllers', [
-    'ngRoute'
-]);
+                                                                  'ngRoute'
+                                                                  ]);
 
 module.config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'views/messaging/index.html',
-            controller: 'ConversationListController'
-        })
-        .when('/conversation/new', {
-        
-        	templateUrl: 'views/messaging/new.html',
-        	controller: 'ConversationNewController'
-        	
-        })
-        .when('/conversation/:id', {
-            templateUrl: 'views/messaging/conversation.html',
-            controller: 'ConversationController'
-        });
-        
+	$routeProvider
+	.when('/', {
+		templateUrl: 'views/messaging/index.html',
+		controller: 'ConversationListController' 
+	})
+	.when('/conversation/new', {
+
+		templateUrl: 'views/messaging/new.html',
+		controller: 'ConversationNewController'
+
+	})
+	.when('/conversation/:id', {
+		templateUrl: 'views/messaging/conversation.html',
+		controller: 'ConversationController'
+	});
+
 });
 
 module.controller('ConversationListController', function($scope, $location, Conversation) {
 
-    // Get all conversations
-    $scope.conversations = Conversation.query();
+	// Get all conversations
+	$scope.conversations = Conversation.query();
 
-    // Change page
-    $scope.changePage = function(path) {
-        console.log(path)
-        $location.path(path);
-    }
+	// Change page
+	$scope.changePage = function(path) {
+		console.log(path)
+		$location.path(path);
+	}
 });
 
 module.controller('ConversationController', function($scope, $routeParams, Conversation) {
-    $scope.conversation = Conversation.get({id: $routeParams.id});
-    // Change page
-    $scope.changePage = function(path) {
-        console.log(path)
-        $location.path(path);
-    }
+	$scope.conversation = Conversation.get({id: $routeParams.id});
+	// Change page
+	$scope.changePage = function(path) {
+		console.log(path)
+		$location.path(path);
+	}
 });
 
 module.controller('ConversationNewController', function($scope, $location, $http, Conversation) {
-	//resouce har API (?)
-	$http.get("http://admin:district@inf5750-19.uio.no/api/users.json").
-	success(function(data, status) {
-      $scope.users = data;
-    }).
-    error(function(data, status) {
-    	alert("ERROR");
-    });
-    // Change page
-    $scope.changePage = function(path) {
-        console.log(path)
-        $location.path(path);
-    }
+	$scope.recv = [];
+	$scope.changed = function() {
+		if($scope.to.length > 0) {
+			$http.get("http://admin:district@inf5750-19.uio.no/api/users?filter=firstName:like:"
+					+ $scope.to).
+					success(function(data, status) {
+						$scope.res = data;
+					}).
+					error(function(data, status) {
+						alert("ERROR");
+					});
+		} else {
+			$scope.res = [];
+		}
+	}
+	$scope.addRecv = function(inp) {
+			$scope.recv[$scope.recv.length] = {name: inp.name,
+					id: inp.id}
+	}
+
+	$scope.remRecv = function(indx) {
+			$scope.recv.splice(indx, 1);    
+	}
+
+	// Change page
+	$scope.changePage = function(path) {
+		console.log(path)
+		$location.path(path);
+	}
 });
