6940b7fb26612c4d5d85eab816dcf4acde2e03b8
Henrik Steen <henrist@henrist.net>
2014-11-14 05:56:17 +0100

    Tabs are not allowed, go away!

--- app/js/messaging/controllers.js
+++ app/js/messaging/controllers.js
@@ -1,38 +1,37 @@
 'use strict';
 
 var module = angular.module('overdressed.messaging.controllers', [
-                                                                  'ngRoute'
-                                                                  ]);
+    'ngRoute'
+]);
 
 module.config(function($routeProvider) {
-	$routeProvider
-	.when('/', {
-		templateUrl: 'views/messaging/index.html',
-		controller: 'ConversationListController' 
-	})
-	.when('/conversation/new', {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/messaging/index.html',
+            controller: 'ConversationListController'
+        })
+        .when('/conversation/new', {
 
-		templateUrl: 'views/messaging/new.html',
-		controller: 'ConversationNewController'
+            templateUrl: 'views/messaging/new.html',
+            controller: 'ConversationNewController'
 
-	})
-	.when('/conversation/:id', {
-		templateUrl: 'views/messaging/conversation.html',
-		controller: 'ConversationController'
-	});
-
+        })
+        .when('/conversation/:id', {
+            templateUrl: 'views/messaging/conversation.html',
+            controller: 'ConversationController'
+        });
 });
 
 module.controller('ConversationListController', function($scope, $location, Conversation) {
 
-	// Get all conversations
-	$scope.conversations = Conversation.query();
+    // Get all conversations
+    $scope.conversations = Conversation.query();
 
-	// Change page
-	$scope.changePage = function(path) {
-		console.log(path)
-		$location.path(path);
-	}
+    // Change page
+    $scope.changePage = function(path) {
+        console.log(path)
+        $location.path(path);
+    }
 });
 
 module.controller('ConversationController', function($scope, $routeParams, Conversation, $window) {
@@ -72,33 +71,33 @@ module.controller('ConversationController', function($scope, $routeParams, Conve
 });
 
 module.controller('ConversationNewController', function($scope, $location, $http, Conversation) {
-	$scope.recv = [];
-	$scope.changed = function() {
-		if($scope.to.length > 0) {
-			$http.get("http://admin:district@inf5750-19.uio.no/api/users?filter=firstName:like:"
-					+ $scope.to).
-					success(function(data, status) {
-						$scope.res = data;
-					}).
-					error(function(data, status) {
-						alert("ERROR");
-					});
-		} else {
-			$scope.res = [];
-		}
-	}
-	$scope.addRecv = function(inp) {
-			$scope.recv[$scope.recv.length] = {name: inp.name,
-					id: inp.id}
-	}
+    $scope.recv = [];
+    $scope.changed = function() {
+        if($scope.to.length > 0) {
+            $http.get("http://admin:district@inf5750-19.uio.no/api/users?filter=firstName:like:"
+                    + $scope.to).
+                    success(function(data, status) {
+                        $scope.res = data;
+                    }).
+                    error(function(data, status) {
+                        alert("ERROR");
+                    });
+        } else {
+            $scope.res = [];
+        }
+    }
+    $scope.addRecv = function(inp) {
+            $scope.recv[$scope.recv.length] = {name: inp.name,
+                    id: inp.id}
+    }
 
-	$scope.remRecv = function(indx) {
-			$scope.recv.splice(indx, 1);    
-	}
+    $scope.remRecv = function(indx) {
+            $scope.recv.splice(indx, 1);
+    }
 
-	// Change page
-	$scope.changePage = function(path) {
-		console.log(path)
-		$location.path(path);
-	}
+    // Change page
+    $scope.changePage = function(path) {
+        console.log(path)
+        $location.path(path);
+    }
 });
