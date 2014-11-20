73c171b160540a43e8b7083ca3e817cfe8cfa9fd
daniehol <daniehol@ifi.uio.no>
2014-11-20 23:50:17 +0100

    Notify a user if we lost internet connection.

--- app/js/app.js
+++ app/js/app.js
@@ -22,3 +22,20 @@ module.config(['$locationProvider', function($locationProvider) {
     //  than there are actually files for, so this is disabled)
     //$locationProvider.html5Mode(true);
 }]);
+
+//Check if web page is offline or not
+module.run(function($window, $rootScope) {
+      $rootScope.offline = navigator.onLine;
+
+      $window.addEventListener("offline", function () {
+        $rootScope.$apply(function() {
+          $rootScope.offline = false;
+        });
+      }, false);
+
+      $window.addEventListener("online", function () {
+        $rootScope.$apply(function() {
+          $rootScope.offline = true;
+        });
+      }, false);
+});
