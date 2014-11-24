d2b666dbcf25efbbf988687df47d16d2249bd082
daniehol <daniehol@ifi.uio.no>
2014-11-24 13:24:41 +0100

    Removed old function

--- app/js/messaging/directives.js
+++ app/js/messaging/directives.js
@@ -9,25 +9,3 @@ module.directive('directiveName', function() {
 
 });
 */
-
-module.directive('activeOnClick',function ($rootScope) {
-	
-	return {
-		link: function($scope, elm) {
-			var tab = angular.element(elm);
-			tab.bind('click', function(e) {
-				if (!tab.hasClass('active')) {
-					tab.addClass('active');
-					$rootScope.$broadcast('elementClicked', tab);
-				}
-			});
-
-			$scope.$on('elementClicked', function(event, theTab){
-				if (!tab)
-					tab = angular.element(elm);
-				if (theTab !== tab && tab.hasClass('active'))
-					tab.removeClass('active');
-			});
-		}
-	}
-});
