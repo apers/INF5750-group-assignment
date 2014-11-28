e1d7ee91c3af151fc85175e3ca0d936fe43aff22
Henrik Steen <henrist@henrist.net>
2014-11-28 05:34:31 +0100

    Moved ConversationController to its own file. Left the route in the main controller file because of route ordering.

--- app/js/messaging/controllers.js
+++ app/js/messaging/controllers.js
@@ -2,7 +2,8 @@
 
 var module = angular.module('overdressed.messaging.controllers', [
     'ngRoute', 'ngTagsInput', 'ui.bootstrap',
-    'overdressed.messaging.controllers.PageController'
+    'overdressed.messaging.controllers.PageController',
+    'overdressed.messaging.controllers.ConversationController'
 ]);
 
 module.config(function ($routeProvider) {
@@ -188,56 +189,6 @@ module.controller('ConversationListController', function ($scope, $location, $ht
             return 'No sender';
         }
     }
-});
-
-module.controller('ConversationController', function ($scope, $routeParams, Conversation, $window, $location) {
-    $scope.conversation = null;
-    Conversation.get($routeParams.id).then(function (ret) {
-        $scope.conversation = ret;
-        console.log("ret", ret);
-    }, function (error) {
-        $scope.conversation = false;
-        console.log("err", error);
-    });
-
-    $scope.changeFollowUp = function () {
-        $scope.conversation.markFollowUp(!$scope.conversation.followUp).then(function (ret) {
-            console.log("followUp success", ret);
-        }, function (err) {
-            console.log("error", err);
-        });
-    };
-
-    $scope.addReply = function () {
-        $scope.reply_text = $scope.reply_text.trim();
-        if ($scope.reply_text == '') return;
-        var text_to_send = $scope.reply_text;
-
-        $scope.conversation.addReply($scope.reply_text).then(function (ret) {
-            if ($scope.reply_text == text_to_send) $scope.reply_text = '';
-            console.log("reply success", ret);
-        }, function (ret) {
-            console.log("reply error", ret);
-        });
-    };
-
-    $scope.markUnread = function () {
-        $scope.conversation.markRead(false).then(function (ret) {
-            console.log("markRead success", ret);
-            $location.path('');
-        }, function (err) {
-            console.log("error", err);
-        });
-    };
-
-    $scope.delete = function () {
-        // TODO
-        console.log("delete conversation", $scope.conversation.id);
-    };
-
-    $scope.gotoTop = function () {
-        $window.scrollTo(0, 0);
-    };
 });
 
 module.controller('ConversationNewController', function ($scope, $location, $http,
