00dc6f9f7af9a7de840efa66ce36d3d1c5f92d25
Henrik Steen <henrist@henrist.net>
2014-12-05 17:42:50 +0100

    Workaround for detecting login requirement

--- app/js/app.js
+++ app/js/app.js
@@ -8,9 +8,30 @@ var module = angular.module('overdressed', [
 ]);
 
 
-module.config(['$routeProvider', function($routeProvider) {
+module.config(function($routeProvider, $httpProvider) {
     // send to index page if unknown url
     $routeProvider.otherwise({
         redirectTo: '/'
     });
-}]);
+
+    // workaround for API bug for catching 401
+    // TODO: change when API get fixed and sends 401
+    console.log("prov", $httpProvider);
+    $httpProvider.interceptors.push(function($window) {
+        return {
+            'response': function (response) {
+                if (typeof response.data == 'string' && response.data.indexOf('class="loginPage"') != -1) {
+                    // only redirect if we were pulling data, not pushing
+                    if (response.config.method == 'GET') {
+                        $window.location.href = '/';
+                    } else {
+                        alert("You are not signed in and cannot perform this action.");
+                    }
+                    return $q.reject(response);
+                }
+
+                return response;
+            }
+        }
+    });
+});
